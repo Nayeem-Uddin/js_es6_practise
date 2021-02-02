7bc5db6dc7934cbb79631c97282740430391fcf6
Nayeem-uddin <nayeemu02@gmail.com>
2021-02-02 16:24:44 +0600

    class

---
+++ class.js
@@ -0,0 +1,12 @@
+class person{
+    constructor(age,name){
+        this.age = age;
+        this.name = name;
+        this.school = 'dhaka international school'
+    }
+}
+const person1 = new person(23,'nayeem');
+const person2 = new person(22,'sadia');
+
+console.log(person1,person2);
+console.log(person1.name);
