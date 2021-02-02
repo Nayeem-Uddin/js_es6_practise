53b77f2d293a4501695ec964dfcca07e9103196e
Nayeem-uddin <nayeemu02@gmail.com>
2021-02-02 15:06:16 +0600

    template nie khela

---
+++ template.js
@@ -0,0 +1,17 @@
+let fName = 'sadia';
+let lName = 'kanchi';
+let mName = 'Islam'
+let fullName = fName+' '+mName+' '+lName;
+//kotto jhamela re baba
+// ES6: I am here dont take panic boy
+let fullName2 = `${fName} ${lName} ${mName} is a good girl, She has ${100+500*5} taka` ;
+console.log(fullName2);
+
+
+// for output multiple line like kobita
+const kobita = 'amar shonar bangla\n'+''+'ami tomai valobashi';
+//make it more simple
+const kobita2 = `amr shoner Bangla
+                ami tomai valobashi`
+
+console.log(kobita2);
